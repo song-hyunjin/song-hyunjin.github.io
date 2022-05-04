71d2bcfee6e08facfe450723168d2b4622502ea8
ssisong <104557486+song-hyunjin@users.noreply.github.com>
2022-05-04 12:48:45 +0900

    Add files via upload

---
+++ 송현진/test01/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.main_slider').slick({
+        arrows:false,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+})
