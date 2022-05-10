a1959c3952ca456e0c5180b827d6dca34eb712db
ssisong <104557486+song-hyunjin@users.noreply.github.com>
2022-05-10 17:23:36 +0900

    Add files via upload

--- test02/js/main.js
+++ test02/js/main.js
@@ -8,10 +8,25 @@ $(function () {
         pauseOnFocus: false,
     });
 
-    // $('.tap a').on('click', function () {
-    //     $('#').show(),
-    //     $()
-    // });
+    $("#myMovie").YTPlayer({
+        videoURL: 'https://youtu.be/vZ0sZffLwnA',
+        containment: '.utube',
+        autoPlay: true,
+        mute: true,
+        startAt: 0,
+        opacity: 1,
+        showControls: false,
+        playOnlyIfVisible: true,
+    });
+
+    $('.utube i:nth-of-type(1)').on('click',function(){
+        $('#myMovie').YTPPlay();
+    })
+
+
+    $('.utube i:nth-of-type(2)').on('click',function(){
+        $('#myMovie').YTPPause();
+    })
 
 
 
