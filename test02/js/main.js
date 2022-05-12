01e8a034bc6597eac8c8d88b9f98a67bd3fad623
ssisong <104557486+song-hyunjin@users.noreply.github.com>
2022-05-12 17:51:01 +0900

    Add files via upload

--- test02/js/main.js
+++ test02/js/main.js
@@ -1,5 +1,35 @@
 $(function () {
 
+    $('.popup button').on('click', function () {
+        $(this).parent().hide();
+    })
+
+
+    $(document).ready(function () {
+        $(".baby_menu").hide();
+        $(".king_menu>a").click(function () {
+            $(this).next().slideToggle(300);
+            $(".king_menu>a").not(this).next().slideUp(300);
+            return false;
+        });
+    });
+
+
+    // $(".menu>li>a").click(function () {
+    //     $(this).next("p").slideToggle(200);
+    //     $(this).parent("li").siblings("li").children("p").slideUp(200);
+    // });
+
+    $('.menu1').click(function () {
+        $('.menu2').slideUp();
+        if ($(this).children('.menu2').is(':hidden')) {
+            $(this).children('.menu2').slideDown();
+        } else {
+            $(this).children('.menu2').slideUp();
+        }
+    });
+
+
     $('.main_event_slider').slick({
         arrows: false,
         dots: true,
@@ -33,8 +63,21 @@ $(function () {
         var idx = $(this).index();
         $(this).addClass('on').siblings().removeClass('on');
         $('.menu_slider_wrapper .menu_slider').eq(idx).addClass('on').siblings().removeClass('on');
-    })
+    });
 
+
+    // $(a).on('clilk', fun(){
+    //     if($(n).hasClass('on')){
+    //         this.next().slto()
+    //     }
+
+    // })
+
+    // $('.king_menu>a').on('click', function () {
+
+    //     $(this).next().slideToggle();
+    //     $(this).siblings().slideUp();
+    // })
 
 
 
